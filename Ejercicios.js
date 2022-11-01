adfcf39574d3d4b295d9006a2de6ddac1bff47fd
ivanhack34 <ivanpolanco34@gmail.com>
2022-11-01 19:34:31 -0400

    primer repaso

---
+++ Ejercicios.js
@@ -0,0 +1,72 @@
+//1-Agregar variables de cadenas
+
+/*const someAdjective = "Nice";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
+
+console.log(myStr);*/
+
+//2-Encuentra la longitud de una cadena
+
+/*
+// Configuración
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Cambia solo el código debajo de esta línea
+lastNameLength = lastName.length;
+
+console.log(lastNameLength);
+*/
+
+//3-Utiliza la notación de corchetes para encontrar el primer carácter en una cadena
+
+/*
+// Configuración
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Cambia solo el código debajo de esta línea
+firstLetterOfLastName = lastName[0]; // Cambia esta línea
+
+console.log(firstLetterOfLastName)
+*/
+
+//4-Comprende la inmutabilidad de las cadenas
+
+/*
+// Configuración
+let myStr = "Jello World";
+
+// Cambia solo el código debajo de esta línea
+myStr= "Hello World"; // Cambia esta línea
+// Cambia solo el código encima de esta línea
+
+console.log(myStr);
+*/
+
+//5-Utiliza la notación de corchetes para encontrar el enésimo carácter en una cadena
+
+/*
+// Configuración
+const lastName = "Lovelace";
+
+// Cambia solo el código debajo de esta línea
+const thirdLetterOfLastName = lastName[2]; // Cambia esta línea
+
+console.log(thirdLetterOfLastName)
+*/
+
+//6-Utiliza la notación de corchetes para encontrar el último carácter en una cadena
+
+/*
+// Configuración
+const lastName = "Lovelace";
+
+// Cambia solo el código debajo de esta línea
+const lastLetterOfLastName = lastName[lastName.length -1]; // Cambia esta línea
+
+console.log(lastLetterOfLastName);
+*/
+
+//7-Utiliza la notación de corchetes para encontrar el carácter enésimo final en una cadena
