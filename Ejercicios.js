718db6eae05b54b0e79171039d940806df591ae5
ivanhack34 <ivanpolanco34@gmail.com>
2022-11-06 18:40:30 -0400

    Ejercicio de practica dia 2 - 6-11-2022

--- Ejercicios.js
+++ Ejercicios.js
@@ -70,3 +70,73 @@ console.log(lastLetterOfLastName);
 */
 
 //7-Utiliza la notación de corchetes para encontrar el carácter enésimo final en una cadena
+
+/*
+// Configuración
+const lastName = "Lovelace";
+
+// Cambia solo el código debajo de esta línea
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Cambia esta línea
+
+console.log(secondToLastLetterOfLastName)
+*/
+
+//8-Palabra en blanco
+
+/*
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Cambia solo el código debajo de esta línea
+const wordBlanks = "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + "."; // Cambia esta línea
+// Cambia solo el código encima de esta línea
+
+console.log(wordBlanks);
+*/
+
+//9-Almacena múltiples valores en una variable utilizando los arreglos de JavaScript
+
+/*
+// Cambia solo el código debajo de esta línea
+const myArray = ['Ivan Polanco', 2];
+
+console.log(myArray)
+*/
+
+//10-Anida un arreglo dentro de otro arreglo
+
+/*
+// Cambia solo el código debajo de esta línea
+const myArray = [['Ivan Polanco', 24], ['Pato', 7]];
+
+console.log(myArray);
+*/
+
+//11-Accede a los datos de un arreglo con índices
+
+/*
+const myArray = [50, 60, 70];
+
+const myData = myArray[0];
+
+console.log(myData)
+*/
+
+//12-Modifica los datos de un arreglo con índices
+
+/*
+// Configuración
+const myArray = [18, 64, 99];
+
+// Cambia solo el código debajo de esta línea
+myArray[0] = 45;
+
+console.log(myArray);
+*/
+
+//Complete en mi segundo dia de hacer ejercicio del 7 al 12 = Total 6 ejercicios simples (6-11-2022) 
+
+//13-Accede a arreglos multidimensionales con índices
+
