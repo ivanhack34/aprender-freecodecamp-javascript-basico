d6cd93a5b5bc43d88ff687ce743cc3c609665a24
ivanhack34 <48842215+ivanhack34@users.noreply.github.com>
2022-12-08 15:47:11 -0400

    Ejercicio del dia 3: 8/12/2022
    Ejercicio del 13 al 20

--- Ejercicios.js
+++ Ejercicios.js
@@ -1,4 +1,4 @@
-//1-Agregar variables de cadenas
+//?1-Agregar variables de cadenas
 
 /*const someAdjective = "Nice";
 let myStr = "Learning to code is ";
@@ -6,82 +6,82 @@ myStr += someAdjective;
 
 console.log(myStr);*/
 
-//2-Encuentra la longitud de una cadena
+//?2-Encuentra la longitud de una cadena
 
 /*
-// Configuración
+//? Configuración
 let lastNameLength = 0;
 const lastName = "Lovelace";
 
-// Cambia solo el código debajo de esta línea
+//? Cambia solo el código debajo de esta línea
 lastNameLength = lastName.length;
 
 console.log(lastNameLength);
 */
 
-//3-Utiliza la notación de corchetes para encontrar el primer carácter en una cadena
+//?3-Utiliza la notación de corchetes para encontrar el primer carácter en una cadena
 
 /*
-// Configuración
+//? Configuración
 let firstLetterOfLastName = "";
 const lastName = "Lovelace";
 
-// Cambia solo el código debajo de esta línea
-firstLetterOfLastName = lastName[0]; // Cambia esta línea
+//? Cambia solo el código debajo de esta línea
+firstLetterOfLastName = lastName[0]; //? Cambia esta línea
 
 console.log(firstLetterOfLastName)
 */
 
-//4-Comprende la inmutabilidad de las cadenas
+//?4-Comprende la inmutabilidad de las cadenas
 
 /*
-// Configuración
+//? Configuración
 let myStr = "Jello World";
 
-// Cambia solo el código debajo de esta línea
-myStr= "Hello World"; // Cambia esta línea
-// Cambia solo el código encima de esta línea
+//? Cambia solo el código debajo de esta línea
+myStr= "Hello World"; //? Cambia esta línea
+//? Cambia solo el código encima de esta línea
 
 console.log(myStr);
 */
 
-//5-Utiliza la notación de corchetes para encontrar el enésimo carácter en una cadena
+//?5-Utiliza la notación de corchetes para encontrar el enésimo carácter en una cadena
 
 /*
-// Configuración
+//? Configuración
 const lastName = "Lovelace";
 
-// Cambia solo el código debajo de esta línea
-const thirdLetterOfLastName = lastName[2]; // Cambia esta línea
+//? Cambia solo el código debajo de esta línea
+const thirdLetterOfLastName = lastName[2]; //? Cambia esta línea
 
 console.log(thirdLetterOfLastName)
 */
 
-//6-Utiliza la notación de corchetes para encontrar el último carácter en una cadena
+//?6-Utiliza la notación de corchetes para encontrar el último carácter en una cadena
 
 /*
-// Configuración
+//? Configuración
 const lastName = "Lovelace";
 
-// Cambia solo el código debajo de esta línea
-const lastLetterOfLastName = lastName[lastName.length -1]; // Cambia esta línea
+//? Cambia solo el código debajo de esta línea
+const lastLetterOfLastName = lastName[lastName.length -1]; //? Cambia esta línea
 
 console.log(lastLetterOfLastName);
 */
 
-//7-Utiliza la notación de corchetes para encontrar el carácter enésimo final en una cadena
+//?7-Utiliza la notación de corchetes para encontrar el carácter enésimo final en una cadena
 
 /*
-// Configuración
+//? Configuración
 const lastName = "Lovelace";
 
-// Cambia solo el código debajo de esta línea
-const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Cambia esta línea
+//? Cambia solo el código debajo de esta línea
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; //? Cambia esta línea
 
 console.log(secondToLastLetterOfLastName)
 */
 
-//8-Palabra en blanco
+//?8-Palabra en blanco
 
 /*
 const myNoun = "dog";
@@ -89,32 +89,32 @@ const myAdjective = "big";
 const myVerb = "ran";
 const myAdverb = "quickly";
 
-// Cambia solo el código debajo de esta línea
-const wordBlanks = "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + "."; // Cambia esta línea
-// Cambia solo el código encima de esta línea
+//? Cambia solo el código debajo de esta línea
+const wordBlanks = "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + "."; //? Cambia esta línea
+//? Cambia solo el código encima de esta línea
 
 console.log(wordBlanks);
 */
 
-//9-Almacena múltiples valores en una variable utilizando los arreglos de JavaScript
+//?9-Almacena múltiples valores en una variable utilizando los arreglos de JavaScript
 
 /*
-// Cambia solo el código debajo de esta línea
+//? Cambia solo el código debajo de esta línea
 const myArray = ['Ivan Polanco', 2];
 
 console.log(myArray)
 */
 
-//10-Anida un arreglo dentro de otro arreglo
+//?10-Anida un arreglo dentro de otro arreglo
 
 /*
-// Cambia solo el código debajo de esta línea
+//? Cambia solo el código debajo de esta línea
 const myArray = [['Ivan Polanco', 24], ['Pato', 7]];
 
 console.log(myArray);
 */
 
-//11-Accede a los datos de un arreglo con índices
+//?11-Accede a los datos de un arreglo con índices
 
 /*
 const myArray = [50, 60, 70];
@@ -124,19 +124,119 @@ const myData = myArray[0];
 console.log(myData)
 */
 
-//12-Modifica los datos de un arreglo con índices
+//?12-Modifica los datos de un arreglo con índices
 
 /*
-// Configuración
+//? Configuración
 const myArray = [18, 64, 99];
 
-// Cambia solo el código debajo de esta línea
+//? Cambia solo el código debajo de esta línea
 myArray[0] = 45;
 
 console.log(myArray);
 */
 
-//Complete en mi segundo dia de hacer ejercicio del 7 al 12 = Total 6 ejercicios simples (6-11-2022) 
+//?Complete en mi segundo dia de hacer ejercicio del 7 al 12 = Total 6 ejercicios simples (6-11-2022) 
 
-//13-Accede a arreglos multidimensionales con índices
+//?13-Accede a arreglos multidimensionales con índices
+
+/*
+const myArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+  ];
+  
+  const myData = myArray[2][1];
+
+  console.log(myData);
+  */
+
+//?14-Manipula arreglos con push()
+
+/*const myArray = [["John", 23], ["cat", 2]];
+myArray.push(["dog", 3])
+
+console.log(myArray);
+*/
+
+//?15-Manipula arreglos con pop()
+
+/*
+const myArray = [["John", 23], ["cat", 2]];
+const removedFromMyArray = myArray.pop();
+
+console.log(removedFromMyArray);
+*/
+
+//?16-Manipula arreglos con shift()
+
+/*const myArray = [["John", 23], ["dog", 3]];
+const removedFromMyArray = myArray.shift();
+
+console.log(removedFromMyArray);
+*/
+
+//?17-Manipula arreglos con unshift()
+
+/*const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+myArray.unshift(["Paul", 35]);
+
+console.log(myArray);
+*/
+
+//?18-Lista de compras
+
+//Crea una lista de compras en la variable myList. 
+//La lista debe ser un arreglo multidimensional que contenga varios sub-arreglos.
+
+//El primer elemento de cada sub-arreglo debe contener una cadena con el nombre del artículo. 
+//El segundo elemento debe ser un número que represente la cantidad, por ejemplo. 
+
+//["Chocolate Bar", 15]
+
+//Debe haber al menos 5 sub-arreglos en la lista.
+
+
+
+/*const myList = [
+    ["padre", 25],
+    ["Marco Polo", 29],
+    ["Jorge Martinez", 29],
+    ["Ivan Polanco", 30],
+    ["Marco Polo", 29]
+  ];
+
+  console.log(myList);
+  */
+
+//?19-Escribe JavaScript reutilizable utilizando funciones
+
+/*function reusableFunction () {
+    console.log("Hi World");
+  }
+  
+  reusableFunction();
+*/
+
+//?20-Pasa valores a las funciones utilizando argumentos
+
+/*function functionWithArgs(a, b) {
+    console.log(a + b);
+  }
+  
+  functionWithArgs(1,2)
+  functionWithArgs(7,9)
+  */
+
+//?21-Devuelve un valor de una función utilizando "Return"
+
+
+
+
+
+
+
 
